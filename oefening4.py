11c79b868749dd8a2e761bc178dfc10c4d4d97d9
Nelly160 <nell.sedeyn@leerling.athenea.be>
2025-03-25 09:18:05 +0100

    huiswerk

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,17 @@
-def countTargetPairs():
+
+def countTargetPairs(nums, target):
+    n = len(nums) 
+    count = 0  
+    
+    for getal in nums: 
+        for i in range(n):
+            for j in range(i + 1, n):  
+                 if nums[i] + nums[j] < target:
+                     count += 1
+    
+    return count
+
+nums = [1, 2, 3, 4]
+target = 5
+result = countTargetPairs(nums, target)
+print("Aantal paren:", result)
